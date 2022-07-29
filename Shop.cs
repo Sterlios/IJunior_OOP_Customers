60eb8e3016916dca3cf5b741bac72ae685373d8d
Sterlios <kuzmin.s.anton@gmail.com>
2022-07-29 11:04:49 +0200

    Refactoring

--- Shop.cs
+++ Shop.cs
@@ -26,23 +26,24 @@ namespace Customers
 
         private void InitializeProducts()
         {
-            _products = new List<Product>();
-
-            _products.Add(new Product(ProductName.Bread, 60));
-            _products.Add(new Product(ProductName.Sugar, 120));
-            _products.Add(new Product(ProductName.Oil, 160));
-            _products.Add(new Product(ProductName.Beer, 135));
-            _products.Add(new Product(ProductName.Limon, 25));
-            _products.Add(new Product(ProductName.Coffie, 700));
-            _products.Add(new Product(ProductName.Tea, 350));
-            _products.Add(new Product(ProductName.Potato, 40));
-            _products.Add(new Product(ProductName.Tomato, 80));
-            _products.Add(new Product(ProductName.Onion, 65));
-            _products.Add(new Product(ProductName.Apple, 85));
-            _products.Add(new Product(ProductName.Banana, 125));
-            _products.Add(new Product(ProductName.Milk, 115));
-            _products.Add(new Product(ProductName.Eggs, 75));
-            _products.Add(new Product(ProductName.Water, 50));
+            _products = new List<Product>
+            {
+                new Product(ProductName.Bread, 60),
+                new Product(ProductName.Sugar, 120),
+                new Product(ProductName.Oil, 160),
+                new Product(ProductName.Beer, 135),
+                new Product(ProductName.Limon, 25),
+                new Product(ProductName.Coffie, 700),
+                new Product(ProductName.Tea, 350),
+                new Product(ProductName.Potato, 40),
+                new Product(ProductName.Tomato, 80),
+                new Product(ProductName.Onion, 65),
+                new Product(ProductName.Apple, 85),
+                new Product(ProductName.Banana, 125),
+                new Product(ProductName.Milk, 115),
+                new Product(ProductName.Eggs, 75),
+                new Product(ProductName.Water, 50)
+            };
         }
     }
 }
